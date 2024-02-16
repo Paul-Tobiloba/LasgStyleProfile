7f604193206da72bdeb53d59a4d1d88ef40c6edb
Paul Tobiloba <48536008+Paul-Tobiloba@users.noreply.github.com>
2024-02-16 11:28:24 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    // Select the second button with class "button" inside the popup-footer-c div
+    $(".popup-footer-c .button:eq(1)").addClass("button-quiet");
+});
