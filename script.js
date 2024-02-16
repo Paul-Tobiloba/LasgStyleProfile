5d791e19f874c46c7abc787eaf74eb1cc4bf199f
Paul Tobiloba <48536008+Paul-Tobiloba@users.noreply.github.com>
2024-02-16 11:41:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
     // Select the second button with class "button" inside the popup-footer-c div
     $(".popup-footer-c .button:eq(1)").addClass("button-quiet");
+    alert("Cancel button clicked!");
 });
